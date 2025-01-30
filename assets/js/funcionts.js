9734ca4e19bdadd2430694c2761078fdc5709841
Natclie <juanrudas2004@gmail.com>
2025-01-30 00:01:17 -0500

    detalles index

--- assets/js/funcionts.js
+++ assets/js/funcionts.js
@@ -18,16 +18,33 @@ document.getElementById("span-dinamico").textContent = palabras[indiceActual];
 
 
 // ABRIR Y CERRAR POP UPS
+
+var abrirpop1 = document.getElementById("sus_med");
+var cerrarpop1 = document.getElementById("sus_med");
+
 function abrir_pop1(){
-    var abrirpop1 = document.getElementById("sus_med");
     abrirpop1.style.top = "10%";
     document.body.style.overflow = 'hidden';
 }
 
-    function cerrar_pop1(){
-        var cerrarpop1 = document.getElementById("sus_med");
-        cerrarpop1.style.top = "-100%";
-        document.body.style.overflow = 'visible';
+function cerrar_pop1(){
+    cerrarpop1.style.top = "-100%";
+    document.body.style.overflow = 'visible';
 }
 
-//CARRUSEL DE GALERIA
+
+// HORA EN TIEMPO REAL PHONE
+function updateTime() {
+    const now = new Date();
+    let horas = now.getHours();
+    let minutos = now.getMinutes();
+    
+    horas = horas < 10 ? "0" + horas : horas;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+
+    document.getElementById("current-time").textContent = `${horas}:${minutos}`;
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
